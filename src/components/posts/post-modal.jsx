5fca20edae848a968dc5b28c127364c6fac9058b
Harshit Jain <harshitostwal1234@gmail.com>
2026-03-20 05:03:00 +0530

    feat: enhance PostModal component with improved layout, styling, and user interactions

--- src/components/posts/post-modal.jsx
+++ src/components/posts/post-modal.jsx
@@ -2,6 +2,10 @@ import { cn } from "@/lib/utils";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Heading } from "../ui/headings";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
+import { X } from "lucide-react";
+import { Input } from "../ui/input";
 
 function PostModal({
   showModal,
@@ -40,52 +44,56 @@ function PostModal({
     <div
       className={cn(
         showModal ? "flex" : "hidden",
-        "fixed inset-0 z-1000 backdrop-blur-xs items-center justify-center",
+        "fixed inset-0 z-1000 backdrop-blur-xs items-center justify-center p-2",
       )}
     >
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="max-w-2xl w-full bg-background p-6 rounded-lg shadow-lg h-96 border border-border flex flex-col gap-4"
+        className="max-w-2xl w-full bg-background p-4 rounded-lg shadow-lg min-h-96 border border-border flex flex-col gap-4"
       >
-        <Heading size="h5">
-          {type === "edit" ? "Edit Post" : "Create a New Post"}
-        </Heading>
-        <Heading size="p" className="text-muted-foreground">
-          {type === "edit"
-            ? "Update your post with new content or images."
-            : "Share your thoughts, code snippets, or anything you want with the community!"}
-        </Heading>
-        <textarea
-          {...register("description", { required: "Description is required" })}
-          placeholder="What's on your mind?"
-          className="w-full h-32 p-3 border border-border rounded-md focus:outline-none focus:ring-2 focus:ring-primary resize-none"
-        />
-        {errors.description && (
-          <p className="text-sm text-red-500 -mt-2">
-            {errors.description.message}
-          </p>
-        )}
-        <input
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center justify-between">
+            <Heading size="h5" className={"font-semibold"}>
+              {type === "edit" ? "Edit Post" : "Create a New Post"}
+            </Heading>
+            <Button
+              onClick={() => setShowModal(false)}
+              variant={"none"}
+              size={"icon"}
+            >
+              <X />
+            </Button>
+          </div>
+          <Heading size="p">
+            {type === "edit"
+              ? "Update your post with new content or images."
+              : "Share your thoughts, code snippets, or anything you want with the community!"}
+          </Heading>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Textarea
+            {...register("description", {
+              required: "Description is required",
+            })}
+            placeholder={"What's on your mind?"}
+          />
+          {errors.description && (
+            <Heading size="p" className="text-destructive">
+              {errors.description.message}
+            </Heading>
+          )}
+        </div>
+
+        <Input
           type="file"
           accept="image/*"
+          className={"file-input file-input-bordered file-input-sm w-full"}
           {...register("image")}
-          className="w-full text-sm text-muted-foreground file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-primary file:text-white hover:file:bg-primary/80 cursor-pointer"
         />
-        <div className="flex justify-end gap-2 mt-auto">
-          <button
-            type="button"
-            onClick={() => setShowModal(false)}
-            className="px-4 py-2 rounded-md border border-border text-muted-foreground hover:bg-secondary"
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            className="px-4 py-2 rounded-md bg-primary text-white hover:bg-primary/80"
-          >
-            {type === "edit" ? "Update Post" : "Create Post"}
-          </button>
-        </div>
+
+        <Button type="submit" className="self-end" size={"sm"}>
+          {type === "edit" ? "Update Post" : "Create Post"}
+        </Button>
       </form>
     </div>
   );
