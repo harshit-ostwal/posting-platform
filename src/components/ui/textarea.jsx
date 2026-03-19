d88e8bcae1a1daa0e6970f0f62309db1474162af
Harshit Jain <harshitostwal1234@gmail.com>
2026-03-20 05:02:49 +0530

    feat: update Input and Textarea components with improved styling and placeholder handling

--- src/components/ui/textarea.jsx
+++ src/components/ui/textarea.jsx
@@ -6,12 +6,12 @@ function Textarea({ className, placeholder, ...props }) {
   return (
     <textarea
       data-slot="textarea"
-      rows={5}
+      placeholder={placeholder}
+      rows={6}
       className={cn(
-        "flex resize-none max-h-96 w-full bg-transparent transition-colors outline-none placeholder:text-muted-foreground  disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 text-lg",
+        "flex resize-none w-full rounded-lg border border-input bg-transparent p-2.5 transition-colors outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
         className,
       )}
-      placeholder={placeholder}
       {...props}
     />
   );
